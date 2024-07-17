329b270bb7688ebf4946db0a775343ced543ed11
chip <58901527+chipwy@users.noreply.github.com>
2024-07-17 16:17:15 -0700

    prediction start

--- hyper_Parm/draw.py
+++ hyper_Parm/draw.py
@@ -1,1 +1,10 @@
-import cv2
+import numpy as np
+
+y_pred = np.asarray([0.4, 0.3, 0.15, 0.15])
+n, e, s, w = np.asarray([0, 1]), np.asarray([1, 0]), np.asarray([0, -1]), np.asarray([-1, 0])
+some_dir = [n, e, s, w]
+
+vecs = [out*dir for out, dir, in zip(y_pred, some_dir)]
+vec_sum = sum(vecs)
+all_dir = [(n+w)/np.sqrt(2), n, (n+e)/np.sqrt(2), e, (e+s)/np.sqrt(2), s, (s+w)/np.sqrt(2), w]
+print(np.argmax([np.dot(vec_sum, dir) for dir in all_dir]) + 1)
